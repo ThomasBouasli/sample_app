a80aaeac05918f57e5e5254f99e4018bae755465
Thomas <thomas.bouasli@gmail.com>
2023-11-21 15:10:55 -0300

    Fix Routing

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_app/pages/home.dart';
 import 'package:sample_app/pages/second.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -25,7 +26,10 @@ class MainDrawer extends StatelessWidget {
           ListTile(
             title: const Text('First Page'),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Home()),
+              );
             },
           ),
           ListTile(
