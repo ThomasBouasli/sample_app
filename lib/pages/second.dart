8b8b0bd367200d1aa18023a77c8940cb97f9646a
Thomas <thomas.bouasli@gmail.com>
2023-11-21 14:25:55 -0300

    Stuff...

--- lib/pages/second.dart
+++ lib/pages/second.dart
@@ -1,9 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:sample_app/data/todo.dart';
 
 class SecondPage extends StatelessWidget {
-  Future<String> getData() async {
-    await Future.delayed(const Duration(seconds: 2));
-    return "Routing from first page";
+  Future<List<Todo>> fetchTodo() async {
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/todos');
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      var decoded = jsonDecode(response.body) as List;
+      return decoded.map((e) => Todo.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load todos');
+    }
   }
 
   final String data;
@@ -23,7 +33,7 @@ class SecondPage extends StatelessWidget {
             ),
             Text(data),
             FutureBuilder(
-              future: getData(),
+              future: fetchTodo(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const CircularProgressIndicator();
@@ -33,11 +43,17 @@ class SecondPage extends StatelessWidget {
                   return Text(snapshot.error.toString());
                 }
 
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(snapshot.data.toString()),
-                  ],
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(snapshot.data![index].title),
+                      trailing: snapshot.data![index].completed
+                          ? const Icon(Icons.check)
+                          : const Icon(Icons.close),
+                    );
+                  },
                 );
               },
             )
